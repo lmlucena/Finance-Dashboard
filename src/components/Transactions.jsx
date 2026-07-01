07af3c1a9f19e522dc517d015e2ac521a944c965
Leonardo Monteiro <lucas.lmlucena@gmail.com>
2026-07-01 13:40:48 -0300

    layout transação finalizada

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,11 +1,11 @@
 const Transactions = () => {
   return (
     <div className="flex gap-5 flex-col bg-BgCard w-[95%] max-w-[1600px] mx-auto rounded-xl p-7">
-      
+      <h2 className="text-md font-semibold ">Nova Transação</h2>
 
-      <div  className="w-full">
-        <h2 className="text-md font-semibold ">Nova Transação</h2>
-        <div className="flex flex-col mb-4">
+      <div  className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-5 gap-5">
+        
+        <div className="flex flex-col w-full mb-4 ">
           <label className="text-CardText font-medium ">Tipo</label>
           <select className="h-10 mt-2 bg-InputBg rounded-lg p-2 pl-5 outline-none border border-BordaInput">
             <option>Receita</option>
@@ -13,7 +13,7 @@ const Transactions = () => {
           </select>
         </div>
 
-        <div className="flex flex-col mb-4">
+        <div className="flex flex-col w-full mb-4">
           <label className="text-CardText font-medium ">Categoria</label>
           <select className="h-10 mt-2 bg-InputBg rounded-lg p-2 pl-5 outline-none border border-BordaInput">
             <option>Salario</option>
@@ -26,9 +26,22 @@ const Transactions = () => {
             <option>Contas</option>
           </select>
         </div>
+
+        <div className="flex flex-col w-full mb-4">
+          <label className="text-CardText font-medium ">Descrição</label>
+          <input type="text" placeholder="Ex: Almoço" className="h-10 mt-2 bg-InputBg rounded-lg p-2 pl-5 outline-none border border-BordaInput"/>
+        </div>
+
+        <div className="flex flex-col w-full mb-4">
+          <label className="text-CardText font-medium ">Valor</label>
+          <input type="number" step="0.01" placeholder="0,00" className="h-10 mt-2 bg-InputBg rounded-lg p-2 pl-5 outline-none border border-BordaInput"/>
+        </div>
+        <div className="flex flex-col w-full mb-4"> <label className="opacity-0 select-none"> botão </label>
+            <button className="h-10 mt-2 w-full bg-[#6366f1] text-white font-semibold rounded-lg border border-BordaInput hover:bg-blue-600 transition-colors"> Adicionar </button> 
+         </div>
         
       </div>
-
+      
     </div>
   )
 }
